62354f8a52e0798688d92a30d5788f550033c868
Morgan <morgan@morganevans.dev>
2022-11-29 12:59:11 +0000

    Updated SingleBook to function

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,30 +2,28 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import CommentArea from "./CommentArea";
 
-class SingleBook extends React.Component {
-    render() {
-        return (
-            <Card
-                style={
-                    this.props.selected
-                        ? {
-                              boxShadow: "0 0 50px 15px #28A745",
-                          }
-                        : {}
-                }
-                bg="dark"
-                className="mb-3 book"
-            >
-                <Card.Img
-                    variant="top"
-                    src={this.props.book.img}
-                    onClick={(e) => {
-                        this.props.setSelected(this.props.book.asin);
-                    }}
-                />
-            </Card>
-        );
-    }
-}
+const SingleBook = (props) => {
+    return (
+        <Card
+            style={
+                props.selected
+                    ? {
+                          boxShadow: "0 0 50px 15px #28A745",
+                      }
+                    : {}
+            }
+            bg="dark"
+            className="mb-3 book"
+        >
+            <Card.Img
+                variant="top"
+                src={props.book.img}
+                onClick={(e) => {
+                    props.setSelected(props.book.asin);
+                }}
+            />
+        </Card>
+    );
+};
 
 export default SingleBook;
