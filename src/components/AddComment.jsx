5e01a80a3766fa0edf228a808a23007d4ef9ced3
Morgan <morgan@morganevans.dev>
2022-11-29 12:56:33 +0000

    Updated AddComment to function

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Form, Col, Button } from "react-bootstrap";
 
 let uri = `https://striveschool-api.herokuapp.com/api/comments/`;
@@ -9,84 +9,70 @@ const opts = {
     },
 };
 
-class AddComment extends React.Component {
-    state = {
-        rating: "1",
-        text: "",
-    };
+const AddComment = (props) => {
+    const [rating, setRating] = useState(1);
+    const [text, setText] = useState("");
 
-    async addComment() {}
+    return (
+        <Form
+            onSubmit={async (e) => {
+                e.preventDefault();
 
-    render() {
-        return (
-            <Form
-                onSubmit={async (e) => {
-                    e.preventDefault();
+                let response = await fetch(uri, {
+                    headers: {
+                        ...opts.headers,
+                        "Content-Type": "application/json",
+                    },
+                    method: "POST",
+                    body: JSON.stringify({
+                        comment: text,
+                        rate: rating,
+                        elementId: props.asin,
+                    }),
+                });
 
-                    let response = await fetch(uri, {
-                        headers: {
-                            ...opts.headers,
-                            "Content-Type": "application/json",
-                        },
-                        method: "POST",
-                        body: JSON.stringify({
-                            comment: this.state.text,
-                            rate: this.state.rating,
-                            elementId: this.props.asin,
-                        }),
-                    });
+                if (response.ok) {
+                    //OK!
+                    props.fetchComments(props.asin);
+                    setText("");
+                    setRating(1);
+                } else {
+                    console.log("Error posting comment");
+                }
+            }}
+        >
+            <Form.Row>
+                <Form.Group as={Col} xs={12} controlId="formGridComment">
+                    <Form.Label>Comment</Form.Label>
+                    <Form.Control
+                        value={text}
+                        onChange={(e) => {
+                            setText(e.target.value);
+                        }}
+                    />
+                </Form.Group>
 
-                    if (response.ok) {
-                        //OK!
-                        this.props.fetchComments(this.props.asin);
-                        this.setState({
-                            ...this.state,
-                            text: "",
-                        });
-                    } else {
-                        console.log("Error posting comment");
-                    }
-                }}
-            >
-                <Form.Row>
-                    <Form.Group as={Col} xs={12} controlId="formGridComment">
-                        <Form.Label>Comment</Form.Label>
-                        <Form.Control
-                            value={this.state.text}
-                            onChange={(e) => {
-                                this.setState({
-                                    ...this.state,
-                                    text: e.target.value,
-                                });
-                            }}
-                        />
-                    </Form.Group>
-
-                    <Form.Group as={Col} controlId="formGridRating">
-                        <Form.Label>Rating</Form.Label>
-                        <Form.Control
-                            as="select"
-                            defaultValue="Choose..."
-                            onChange={(e) => {
-                                this.setState({
-                                    ...this.state,
-                                    rating: e.target.value,
-                                });
-                            }}
-                        >
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </Form.Control>
-                    </Form.Group>
-                </Form.Row>
-                <button type="submit">Submit</button>
-                <hr />
-            </Form>
-        );
-    }
-}
+                <Form.Group as={Col} controlId="formGridRating">
+                    <Form.Label>Rating</Form.Label>
+                    <Form.Control
+                        as="select"
+                        defaultValue="Choose..."
+                        onChange={(e) => {
+                            setRating(e.target.value);
+                        }}
+                    >
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Form.Control>
+                </Form.Group>
+            </Form.Row>
+            <button type="submit">Submit</button>
+            <hr />
+        </Form>
+    );
+};
 
 export default AddComment;
